2068d15bb991d5105d3eed1db440a3954b664977
Sean Qian <Shengxin.Qian@ibm.com>
2018-09-04 20:30:41 -0400

    add dateUtils.java in common project

--- UVDS-Common/src/main/java/com/parabird/uvds/common/dateUtils/dateUtils.java
+++ UVDS-Common/src/main/java/com/parabird/uvds/common/dateUtils/dateUtils.java
@@ -1,5 +1,8 @@
 package com.parabird.uvds.common.dateUtils;
 
 public class dateUtils {
-    public static final String dateFormat = 
+    //We use ISO 8601 time for entire project
+    public static final String dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ";
+
+
 }
