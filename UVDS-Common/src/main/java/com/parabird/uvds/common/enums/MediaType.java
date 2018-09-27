bcb70623f137aeb9bbdbe2c75d0402f56d29049f
Sean Qian <Shengxin.Qian@ibm.com>
2018-09-27 10:31:35 -0400

    add general media type in UVDS-Common

--- UVDS-Common/src/main/java/com/parabird/uvds/common/enums/MediaType.java
+++ UVDS-Common/src/main/java/com/parabird/uvds/common/enums/MediaType.java
@@ -1,6 +1,7 @@
 package com.parabird.uvds.common.enums;
 
 public enum MediaType {
+    GENERAL,
     IMAGE,
     VIDEO,
     TEXT,
--- UVDS-Common/src/main/java/com/parabird/uvds/common/utils/FileUtils.java
+++ UVDS-Common/src/main/java/com/parabird/uvds/common/utils/FileUtils.java
@@ -91,4 +91,5 @@ public class FileUtils {
 
         return lines;
     }
+
 }
